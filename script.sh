faf2343482c2e291a00c91ab7c9049e51e6a3351
DAbrigo <di.abrigo@duocuc.cl>
2025-05-01 22:04:39 -0400

    NOSE QUE WEA ESTOY HACIENDO 4

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@ echo "RUN apt-get install -y python3-pip" >> Dockerfile
 echo "RUN pip install flask" >> Dockerfile 
 echo "COPY index.html /home/myapp/templates/" >> Dockerfile 
 echo "COPY sample_app.py /home/myapp/" >> Dockerfile 
-echo "EXPOSE 9000" >> Dockerfile 
+echo "EXPOSE 8000" >> Dockerfile 
 echo "CMD python3 /home/myapp/sample_app.py" >> Dockerfile
 
 docker build -t web .
-docker run -d -p 9000:9000 --name web web
+docker run -d -p 8000:8000 --name web web
 docker ps 
