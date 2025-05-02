b1e45b8201d70bbfd856fedfb7406b235e52fb0e
DAbrigo <di.abrigo@duocuc.cl>
2025-05-01 21:51:31 -0400

    NOSE QUE WEA ESTOY HACIENDO 2

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@ echo "COPY index.html /home/myapp/templates/" >> Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> Dockerfile 
 echo "EXPOSE 9000" >> Dockerfile 
 echo "CMD python3 /home/myapp/sample_app.py" >> Dockerfile
+
 docker build -t web .
 docker run -d -p 9000:9000 --name web web
 docker ps 
